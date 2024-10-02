9c6c172179108a2ac10115e08038e2bbbe22b34c
BuildTools <unconfigured@null.spigotmc.org>
2024-10-02 11:20:55 -0500

    theme template purge

--- src/app/Nameless/page.js
+++ src/app/Nameless/page.js
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 import { Box, Typography } from "@mui/material"
-import {themeOptions} from "../page"
+//import {themeOptions} from "../page"
 export default function Nameless()
 {
-    return(<Box sx={{marginLeft: '25%', width: '50%', background: themeOptions.palette.primary.light}}>
+    return(<Box sx={{marginLeft: '25%', width: '50%', background: '#546654'/* themeOptions.palette.primary.light */}}>
         <h1 style={{marginLeft: '20%', paddingTop: 10}}>Nameless</h1>
         <Image src='/Images/temp_nameless_promo.png' width={640} height={360} style={{marginLeft: '20%', marginBottom: 10}}/>
         <h3 style={{marginLeft: '20%'}}>Overview</h3>
-        <Typography sx={{mx: 5}}>Nameless is my current, and biggest project to date. It's a standard Action RPG with a main focus on monster slaying and crafting.</Typography>
+        <Typography sx={{mx: 5}}>Nameless is my current, and biggest project to date. It&apos;s a standard Action RPG with a main focus on monster slaying and crafting.</Typography>
     </Box>)
 }
