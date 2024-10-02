f3111f36e30de1543f538f1b5c3e0f054f67e6d5
BuildTools <unconfigured@null.spigotmc.org>
2024-10-02 16:05:06 -0500

    restore layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" >
-      <body>{children}</body>
+      <NavBar />
+      <body style={{marginTop: 65}}>{children}</body>
     </html>
   );
 }
