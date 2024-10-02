bb91fa515d868cb46176cb1c8100e3f1bacd1d96
BuildTools <unconfigured@null.spigotmc.org>
2024-10-02 16:57:55 -0500

    build debugging

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" >
-      
+      <NavBar />
       <body style={{marginTop: 65}}>{children}</body>
     </html>
   );
