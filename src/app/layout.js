e5598a6922dd5e9c643da33906445c3ac095ac77
BuildTools <unconfigured@null.spigotmc.org>
2024-10-03 09:18:05 -0500

    restore backround color, update navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" >
+    <html lang="en" style={{backgroundColor: '#080d08'}} >
       <NavBar />
       <body style={{marginTop: 65}}>{children}</body>
     </html>
