b0e4bd7c9b37f684b8e6803f9ad06d8e8fdb8bef
BuildTools <unconfigured@null.spigotmc.org>
2024-10-03 09:24:02 -0500

    fixed games/about page background

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -1,11 +1,11 @@
 import Post from "../_components/post";
 export default function Projects(){
     return(
-        <>
+        <div>
             <Post postIcon={"temp_nameless_icon"} postTitle={"Nameless"}
             postText= {"I&apos;m the sole developer on the project; Doing everything from planning and design, to engineering and art."}
             ></Post>
             <Post postIcon={"X"} postTitle={"Test Post"} postText={"This is also a test post"}></Post>
-        </>
+        </div>
     )
 }
