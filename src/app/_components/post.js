d3a1715ced61e3df95b8fe1ccaa706b51a2986a7
BuildTools <unconfigured@null.spigotmc.org>
2024-10-03 16:22:13 -0500

    build debugging

--- src/app/_components/post.js
+++ src/app/_components/post.js
@@ -8,7 +8,7 @@ export default function Post({postIcon, postTitle, postText}){
         <Link href={"/"+postTitle+".html"}>
             <Box sx={{mx: 'auto', mb: 10, background: '#2a402a'/* themeOptions.palette.primary.main */, minWidth: 200, maxWidth: 800, height: 200, borderRadius: 10, boxShadow: 4, display: 'flex'}}> {/* outer post component */}
                 <Box sx={{background: '#1D2C1D'/* themeOptions.palette.primary.dark */, minWidth: 150, maxWidth: 750, height: 150, mt: 3, ml: 3, display: 'flex', borderRadius: 3}}> {/* post icon */}
-                    <Image src={X} width={140} height={140} style={{paddingLeft: 10, paddingTop: 10, borderRadius: 3}} alt={postIcon}/>
+                    <img src={X} width={140} height={140} style={{paddingLeft: 10, paddingTop: 10, borderRadius: 3}} alt={postIcon}/>
                 </Box>
                 <Box sx={{background: "#5d805d", ml: 3, my: 3, height: 140, minWidth: '60%', maxWidth: '70%', borderRadius: 3}}>{/* post text */}
                     <Typography variant="h4" sx={{mx: 20, color: 'black', fontWeight: 'bold'}}>{postTitle}</Typography>
