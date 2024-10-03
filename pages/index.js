4fe9a59ccd550266d22e9d13dcf7319be1499af9
BuildTools <unconfigured@null.spigotmc.org>
2024-10-03 15:04:23 -0500

    update routing and layout

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import RootLayout from './layout'
+import { Box, Typography } from "@mui/material"
+
+/* export const themeOptions = {
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#2a402a',
+        dark: '#1D2C1D',
+        light: '#546654',
+      },
+      secondary: {ß
+        main: '#f50057',
+        dark: '#36695D',
+      },
+      info: {
+        main: '#882d71',
+      },
+      success: {
+        main: 'rgb(53, 122, 56)',
+      },
+    },
+}; */
+
+export default function HomePage()
+{
+    return(
+      <RootLayout>
+        <Box sx={{marginLeft: '25%', marginRight: '25%', background: '#1D2C1D'/* themeOptions.palette.primary.dark */}}>
+            <h1 style={{marginLeft: '25%', marginRight: '25%'}}>Welcome!</h1>
+            <p style={{marginLeft: '25%', marginRight: '25%'}}>Thanks for stopping by. Stay for a while and take a look around, I&apos;m sure you&apos;ll find something interesting.</p>
+        </Box>
+        </RootLayout>
+    )
+}
---
+++ pages/layout.js
@@ -0,0 +1,17 @@
+import { Inter } from "next/font/google";
+import "/src/app/globals.css";
+import NavBar from "/src/app/_components/navbar";
+import { red } from "@mui/material/colors";
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+
+export default function RootLayout({ children }) {
+  return (
+    <>
+      <NavBar />
+      <main style={{marginTop: 65}}>{children}</main>
+    </>
+  );
+}
